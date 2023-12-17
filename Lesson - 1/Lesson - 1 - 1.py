252b58c7fd537408f7ecd639f2e68372963b6de5
Uladzisla Savitskiy <uladzislau.savitskiy@outlook.com>
2023-12-18 00:46:42 +0300

    Add Lesson - 1

---
+++ Lesson - 1/Lesson - 1 - 1.py
@@ -0,0 +1,8 @@
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+if num1 > num2:
+  print(True)
+elif num1 == num2:
+  print('Числа равны')
+else:
+  print(False)
